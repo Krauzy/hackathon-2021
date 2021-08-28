947075fcb9f9fcc927ef2e07ee5f43ac2afbe85d
LPicinin <luishpj@outlook.com.br>
2021-08-28 19:52:55 -0300

    Detecção de poses e reconhecimento de expressões faciais
    as expressões parecem estar uns 70% corretas

--- mediapipe/demo.py
+++ mediapipe/demo.py
@@ -1,12 +1,17 @@
 import cv2
 import mediapipe as mp
+from index import FaceExpression
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
 # For webcam input:
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+flag = False
+
+fe = FaceExpression()
+
 with mp_holistic.Holistic(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as holistic:
@@ -16,7 +21,6 @@ with mp_holistic.Holistic(
             print("Ignoring empty camera frame.")
             # If loading a video, use 'break' instead of 'continue'.
             continue
-
         # Flip the image horizontally for a later selfie-view display, and convert
         # the BGR image to RGB.
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
@@ -24,28 +28,32 @@ with mp_holistic.Holistic(
         # pass by reference.
         image.flags.writeable = False
         results = holistic.process(image)
+        image = fe.getExpression(image)
 
         # Draw landmark annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        mp_drawing.draw_landmarks(
-            image,
-            results.face_landmarks,
-            mp_holistic.FACEMESH_CONTOURS,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp_drawing_styles
-            .get_default_face_mesh_contours_style())
+        if flag:
+            image[:] = (0, 0, 0)
+
+        # mp_drawing.draw_landmarks(
+        #     image,
+        #     results.face_landmarks,
+        #     mp_holistic.FACEMESH_CONTOURS,
+        #     landmark_drawing_spec=None,
+        #     connection_drawing_spec=mp_drawing_styles
+        #     .get_default_face_mesh_contours_style())
         # otimiza não plotando a malha da face
-        '''
-            mp_drawing.draw_landmarks(
-            image,
-            results.face_landmarks,
-            mp_holistic.FACEMESH_TESSELATION,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp_drawing_styles
-            .get_default_face_mesh_tesselation_style())
-        '''
+
+        # mp_drawing.draw_landmarks(
+        #     image,
+        #     results.face_landmarks,
+        #     mp_holistic.FACEMESH_TESSELATION,
+        #     landmark_drawing_spec=None,
+        #     connection_drawing_spec=mp_drawing_styles
+        #     .get_default_face_mesh_tesselation_style())
+
         # Otimização - caso comentando a duas linhas abaixo, o programa não plota as malhas das mãos
         mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
         mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
@@ -55,9 +63,14 @@ with mp_holistic.Holistic(
             results.pose_landmarks,
             mp_holistic.POSE_CONNECTIONS,
             landmark_drawing_spec=mp_drawing_styles
-            .get_default_pose_landmarks_style())
+                .get_default_pose_landmarks_style())
 
-        cv2.imshow('MediaPipe Holistic', image)
-        if cv2.waitKey(5) & 0xFF == 27:
+
+        cv2.imshow('Holistic', image)
+        key = cv2.waitKey(5) & 0xFF
+        if key == 27:
             break
+        elif key == ord(' '):
+            flag = not flag
+
 cap.release()
